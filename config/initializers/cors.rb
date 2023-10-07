e86f91c69251f81c80e9f3996b66fd60cf248cb6
mdelvecchio <mdelvecchio3119@gmail.com>
2023-10-07 15:25:54 -0400

    Update origins in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://game-idea-manager-frontend.onrender.com/"
+    origins "https://game-idea-manager-frontend.onrender.com"
 
     resource "*",
       headers: :any,
